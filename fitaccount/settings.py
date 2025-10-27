6aef3e0a17c7834d7cfa5cabe52dd86e64df71ec
jayS202 <63660559+jayS202@users.noreply.github.com>
2025-10-27 15:19:12 +0530

    Update settings.py

--- fitaccount/settings.py
+++ fitaccount/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-gb_1^y1kky_$44vlua#ox0%zurhk==_7fh-^^o)2a+6x(tp=fc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Samashk.pythonanywhere.com']
 
 
 # Application definition
